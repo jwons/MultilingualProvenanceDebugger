825f92709b889833a3e0d069fa8c8d4594a34885
Joe <joe.wonsil@gmail.com>
2019-02-24 15:37:00 -0600

    commenting for clarity

--- provdebug/ProvBrowser.py
+++ provdebug/ProvBrowser.py
@@ -23,24 +23,43 @@ class ProvBrowser:
         # will be the next node to advance to when a user prompts 
         self.procProcEdges = self.debugger.prov.getProcProc()
 
+        # This variable keeps track of where the simulated execution is in terms of 
+        # scope that is possible to step in and out of. It merely represents an index
+        # to a list of procedure nodes
         self._currentScope = 0
 
+        # These are used to keep track of positions when traveling
+        # through the procedure nodes one at a time initially and 
+        # organizing them into different scopes. 
         numOfScopes = 0
         lastScope = []
 
+        # The scope stack will represent the different scopes that exist 
+        # within the simulated execution. It is a list of lists, where the 
+        # inner lists contain a dictionary with procedure nodes and possible scope changes
         self._scopeStack = []
         self._scopeStack.append([])
 
         procNodes = self.debugger.prov.getProcNodes()
 
+        # This loop makes an initial iteration through all of the procedure 
+        # nodes and organizes each one into its respective "scope"
         for index, row in procNodes.iterrows():
             
+            # Start nodes indicate the possibility of a new scope could
+            # be stepped into at this node. Therefore, when appending the 
+            # node to the scopeStack, indicate with the scopeChange dict 
+            # element that index value into the scopeStack could be traeversed 
+            # to during the program reconstruction
             if(row['type'] == "Start"):
                 self._scopeStack.append([])
                 self._scopeStack[self._currentScope].append({"row":row, "scopeChange":numOfScopes + 1})
                 lastScope.append(self._currentScope)
                 numOfScopes += 1
                 self._currentScope = numOfScopes
+            # A finish node indicates that the last node will be where the 
+            # reconstrucuted execution will leave the scope to traverse back to 
+            # the one it was in previosuly 
             elif(row["type"] == "Finish"):
                 self._scopeStack[self._currentScope][-1]["scopeChange"] = lastScope[-1]
                 self._currentScope = lastScope[-1]
@@ -49,15 +68,6 @@ class ProvBrowser:
                 del lastScope[-1]
             else:
                 self._scopeStack[self._currentScope].append({"row":row, "scopeChange":self._currentScope})  
-
-        '''
-        counter = 0
-        for scope in self._scopeStack:
-            print("Scope number: ", counter)
-            for row in scope:
-                print(row["row"]["label"], "| goes to:", row["scopeChange"])
-            counter += 1
-        '''
 
         # Reset to ensure simulated code will start at beginning
         self._currentScope = 0
@@ -70,7 +80,12 @@ class ProvBrowser:
     def getCurrentNodeInfo(self):
         return(self._scopeStack[self._currentScope][self.currentNodeIndex]["row"])
 
+    # This function sets the node information to be that of a node a single scope deeper, 
+    # if possible. Otherwise it will just advance forward one node in the current scope
     def stepIn(self):
+        # Check if the scope change stored in the dict points to a different scope than the current 
+        # If it does the set all the current indexes to the next scope. Additionally, keep track 
+        # of where the program entered to get back to it later.
         if(self._scopeStack[self._currentScope][self.currentNodeIndex]["scopeChange"] != self._currentScope):
             self.positionStack.append({"scope": self._currentScope, "node":self.currentNodeIndex})
             self._currentScope = self._scopeStack[self._currentScope][self.currentNodeIndex]["scopeChange"]
