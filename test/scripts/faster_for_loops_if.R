d7c00995b1c536fa5476692afaf803c9827fae25
James Yoo <jyoo980@gmail.com>
2021-03-27 18:19:28 -0700

    adding original R scripts

---
+++ test/scripts/faster_for_loops_if.R
@@ -0,0 +1,17 @@
+# To get faster for loops in R, we need to 
+# pre-allocate the memory, using length():
+
+outcome_object <- c(NA)
+length(outcome_object ) <- 100000
+
+for(i in 1:100000) {
+  outcome_object[i] <- i
+  }
+
+x <- 3
+
+if (x == 2) {
+	print("WAS EQUAL TO TWO")
+} else {
+	print("WAS NOT EQUAL TO TWO")
+}	
---
+++ test/scripts/faster_for_loops_same_len.R
@@ -0,0 +1,13 @@
+# To get faster for loops in R, we need to 
+# pre-allocate the memory, using length():
+
+outcome_object <- c(NA)
+length(outcome_object ) <- 100000
+
+
+
+if (TRUE) {
+	print("HELLO")
+} else {
+	print("WORLD")
+}
